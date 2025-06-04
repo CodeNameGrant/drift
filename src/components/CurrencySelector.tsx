73f449d6e87de70677ad4ffd8d1dbdf615ef25bd
CodeNameGrant <GrantW2505@gmail.com>
2025-06-04 21:16:07 +0200

    Updated CurrencySelector.tsx

--- src/components/CurrencySelector.tsx
+++ src/components/CurrencySelector.tsx
@@ -15,7 +15,7 @@ const CurrencySelector: React.FC = () => {
     >
       {currencies.map(c => (
         <option key={c.code} value={c.code}>
-          {c.code} {c.symbol}
+          {c.code} - {c.name}
         </option>
       ))}
     </select>
