fb07ead63cbf63db76b5e943b6b38f3ec579c75e
CodeNameGrant <GrantW2505@gmail.com>
2025-06-07 10:38:58 +0200

    Create currency icon selector component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LoanCalculator from './components/LoanCalculator';
 import ThemeToggle from './components/ThemeToggle';
 import GitHubLink from './components/GitHubLink';
-import CurrencySelector from './components/CurrencySelector';
+import CurrencyIconSelector from './components/CurrencyIconSelector';
 import { useTheme } from './hooks/useTheme';
 import { CurrencyProvider } from './context/CurrencyContext';
 
@@ -12,11 +12,11 @@ function App() {
   return (
     <CurrencyProvider>
       <div className="min-h-screen bg-appBackground-light dark:bg-appBackground-dark text-gray-900 dark:text-white transition-colors duration-300">
-        <div className="fixed top-5 left-5 z-50">
-          <CurrencySelector />
+        <div className="fixed top-4 right-4 flex items-center gap-2 z-50">
+          <CurrencyIconSelector />
+          <GitHubLink />
+          <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
         </div>
-        <GitHubLink />
-        <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
         <div className="container mx-auto py-12 px-4">
           <LoanCalculator />
         </div>
