5d2dd4211181d2886993382af12808f8dcdf92a5
CodeNameGrant <GrantW2505@gmail.com>
2025-06-07 10:41:31 +0200

    Updated GitHubLink.tsx

--- src/components/GitHubLink.tsx
+++ src/components/GitHubLink.tsx
@@ -10,7 +10,7 @@ const GitHubLink: React.FC = () => {
       className="p-2 rounded-full bg-gray-200 dark:bg-gray-800 transition-colors duration-300 hover:bg-gray-300 dark:hover:bg-gray-700"
       aria-label="View source code on GitHub"
       title="View source code on GitHub"
-    >
+    >Test1
       <Github className="h-5 w-5 text-gray-800 dark:text-gray-200" />
     </a>
   );
